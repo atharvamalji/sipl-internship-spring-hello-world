efcfa248021f8623bbd4e290d639703a228ece6e
Atharva Malji <atharva.malji.2001@gmail.com>
2023-10-09 19:08:22 +0530

    updated controller name to HomeController

--- src/main/java/com/sipl/springhelloworld/controllers/HomeController.java
+++ src/main/java/com/sipl/springhelloworld/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-public class Controller {
+public class HomeController {
     
     // hello world api basepoint
     @GetMapping("/hello")
